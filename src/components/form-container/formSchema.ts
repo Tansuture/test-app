cea174fa6e72c911d1b74f8d767f63994737da0a
t.turemuratova <t.turemuratova@qtap.ai>
2025-01-26 14:36:01 +0500

    add validation for email,phone,index

--- src/components/form-container/formSchema.ts
+++ src/components/form-container/formSchema.ts
@@ -1,22 +1,31 @@
-import {mixed, object, ObjectSchema, string} from "yup";
+import { mixed, object, ObjectSchema, string } from "yup";
 
-export const formSchema = (): ObjectSchema<any> =>object().shape({
-  personalInfo:object().shape({
-    name:string().required('Поле обязательное'),
-    surName:string().required('Поле обязательное'),
-    dateValue:string().required('Поле обязательное'),
-    email:string().email().required('Поле обязательное'),
-    number:string().required('Поле обязательное')
-  }),
-  addressInfo:object().shape({
-    city:string().required('Поле обязательное'),
-    country:mixed().required('Поле обязательное'),
-    street:string().required('Поле обязательное'),
-    index:string().required('Поле обязательное')
-  }),
-  financeInfo:object().shape({
-    income:string().required('Поле обязательное'),
-    amount:string().required('Поле обязательное'),
-    term:string().required('Поле обязательное'),
-  })
-})
+export const formSchema = (): ObjectSchema<any> =>
+  object().shape({
+    personalInfo: object().shape({
+      name: string().required("Поле обязательное"),
+      surName: string().required("Поле обязательное"),
+      dateValue: string().required("Поле обязательное"),
+      email: string()
+        .email("Неверный формат email")
+        .required("Поле обязательное"),
+      number: string()
+        .required("Поле обязательное")
+        .min(10, "Номер телефона должен соблюдать 11 чисел")
+        .max(10, "Номер телефона должен соблюдать 1 чисел"),
+    }),
+    addressInfo: object().shape({
+      city: string().required("Поле обязательное"),
+      country: mixed().required("Поле обязательное"),
+      street: string().required("Поле обязательное"),
+      index: string()
+        .required("Поле обязательное")
+        .min(5, "Индекс должен соблюдать минимум 5 чисел")
+        .max(10, "Индекс должен соблюдать минимум 10 чисел"),
+    }),
+    financeInfo: object().shape({
+      income: string().required("Поле обязательное"),
+      amount: string().required("Поле обязательное"),
+      term: string().required("Поле обязательное"),
+    }),
+  });
--- src/components/shared/text-field.tsx
+++ src/components/shared/text-field.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
-import {FormInputProps} from "../../types/form-input-props";
+import React from "react";
+import { FormInputProps } from "../../types/form-input-props";
 import { Controller } from "react-hook-form";
 import TextField from "@mui/material/TextField";
 
-export const FormInputText = ({ name, control, label,type }: FormInputProps) => {
+export const FormInputText = ({
+  name,
+  control,
+  label,
+  type,
+}: FormInputProps) => {
   const handleOnChange = (
     e: React.ChangeEvent<HTMLInputElement>,
     onChange: (value: string) => void
@@ -11,15 +16,15 @@ export const FormInputText = ({ name, control, label,type }: FormInputProps) => 
     const value = e.target.value;
     let sanitizedValue = value;
 
-    switch(type) {
-      case 'text':
-        sanitizedValue = value.replace(/[^a-zA-Zа-яА-Я\s]/g, '');
+    switch (type) {
+      case "text":
+        sanitizedValue = value.replace(/[^a-zA-Zа-яА-Я\s]/g, "");
         break;
-      case 'number':
-        sanitizedValue = value.replace(/[^0-9]/g, '');
+      case "number":
+        sanitizedValue = value.replace(/[^0-9]/g, "");
         break;
-      case 'email':
-        sanitizedValue = value.toLowerCase().replace(/[^a-z0-9@._-]/g, '');
+      case "email":
+        sanitizedValue = value.toLowerCase().replace(/[^a-z0-9@._-]/g, "");
         break;
     }
 
@@ -29,23 +34,18 @@ export const FormInputText = ({ name, control, label,type }: FormInputProps) => 
     <Controller
       name={name}
       control={control}
-      render={({
-                 field: { onChange, value },
-                 fieldState: { error },
-
-               }) => (
+      render={({ field: { onChange, value }, fieldState: { error } }) => (
         <TextField
           helperText={error ? error.message : null}
           size="small"
           error={!!error}
-          onChange={(e:React.ChangeEvent<HTMLInputElement>) => {
-           handleOnChange(e,onChange)
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            handleOnChange(e, onChange);
           }}
           value={value}
           fullWidth
           label={label}
-          variant='outlined'
-
+          variant="outlined"
         />
       )}
     />
